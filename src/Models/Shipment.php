2e4fb0a2b7a21044e0f3b0e485484402082298d1
railken <railken@protonmail.ch>
2019-05-11 17:07:34 +0200

    fix style

--- src/Models/Shipment.php
+++ src/Models/Shipment.php
@@ -10,7 +10,8 @@ use Railken\Lem\Contracts\EntityContract;
 
 class Shipment extends Model implements EntityContract
 {
-    use SoftDeletes, ConfigurableModel;
+    use SoftDeletes;
+    use ConfigurableModel;
 
     /**
      * Create a new Eloquent model instance.
