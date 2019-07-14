8f5e0ddf64b37b950215d94415b2ed80bc574d4e
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/ShipmentManager.php
+++ src/Managers/ShipmentManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\Shipment newEntity()
+ * @method \Amethyst\Schemas\ShipmentSchema getSchema()
+ * @method \Amethyst\Repositories\ShipmentRepository getRepository()
+ * @method \Amethyst\Serializers\ShipmentSerializer getSerializer()
+ * @method \Amethyst\Validators\ShipmentValidator getValidator()
+ * @method \Amethyst\Authorizers\ShipmentAuthorizer getAuthorizer()
+ */
 class ShipmentManager extends Manager
 {
     use ConfigurableManager;
